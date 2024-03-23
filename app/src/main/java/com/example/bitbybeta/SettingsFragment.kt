524d245b058ede1e27330988fc943df6c51f740b
jackbeav <jackbeaver0+github@gmail.com>
2024-03-23 15:56:24 -0500

    basic settings fragment complete
    temp navigation from first fragment due to no home page

--- app/src/main/java/com/example/bitbybeta/SettingsFragment.kt
+++ app/src/main/java/com/example/bitbybeta/SettingsFragment.kt
@@ -6,22 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.bitbybeta.databinding.FragmentSecondBinding
+import com.example.bitbybeta.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.notificationToggleSwitch.setOnClickListener {
+            //toggle the setting
         }
     }
 
