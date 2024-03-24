1303556c19c9b495857eb1f8c8291a9c0420d60a
jackbeav <jackbeaver0+github@gmail.com>
2024-03-24 14:08:03 -0500

    Filled out settings
    still need to link them to actual values under each set

--- app/src/main/java/com/example/bitbybeta/SettingsFragment.kt
+++ app/src/main/java/com/example/bitbybeta/SettingsFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.bitbybeta.databinding.FragmentSecondBinding
 import com.example.bitbybeta.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -14,11 +14,13 @@ class SettingsFragment : Fragment() {
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -29,9 +31,32 @@ class SettingsFragment : Fragment() {
         binding.notificationToggleSwitch.setOnClickListener {
             //toggle the setting
         }
+
+        //set up drop down menu items
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.time_setting_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            binding.startTimeSpinner.adapter = adapter
+        }
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.time_setting_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            binding.endTimeSpinner.adapter = adapter
+        }
     }
 
     override fun onDestroyView() {
+        //save settings to whatever needs to be saved
         super.onDestroyView()
         _binding = null
     }
